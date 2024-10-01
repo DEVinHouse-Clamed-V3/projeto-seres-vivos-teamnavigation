48591dcb454772622f3c4a420fd10f842378bd5c
nataliaclem <nataliaclementinosilva@gamil.com>
2024-10-01 19:34:50 -0300

    plantas atualização

--- src/pages/Plantas.js
+++ src/pages/Plantas.js
@@ -1,13 +1,60 @@
-import React from "react";
-import { Text, SafeAreaView } from "react-native";
-import { globalStyles } from "../styles/global";
+import React, { useState, useEffect } from "react"; 
+import {globalStyles} from "../styles/global";
+import {
+  Text,
+  SafeAreaView,
+  FlatList,
+  View,
+  Image,
+  Alert,
+} from "react-native";
+import axios from "axios";
 
 const Plantas = () => {
+  const [plantas, setPlantas] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://192.168.112.26:3000/plantas")
+      .then((response) => {
+        setPlantas(response.data);
+      })
+      .catch(() => {
+        Alert.alert("tempo encerrado");
+      });
+  }, []);
+
+  const plantasItem = ({ item }) => (
+    <View style={globalStyles.cartao}>
+      <Image source={{ uri: item.image }} style={globalStyles.imagemCartao} />
+      <View style={globalStyles.conteudoCartao}>
+        <Text style={globalStyles.nomeCartao}>{item.name}</Text>
+        <Text style={globalStyles.descricaoCartao}>{item.description}</Text>
+        <Text style={globalStyles.detalheCartao}>Nutrição: {item.nutrition}</Text>
+        <Text style={globalStyles.detalheCartao}>Respiração: {item.respiration}</Text>
+        <Text style={globalStyles.detalheCartao}>
+          Tipo de Célula: {item.cellType}
+        </Text>
+        <Text style={globalStyles.detalheCartao}>
+          Organização Celular: {item.cellOrganization}
+        </Text>
+        <Text style={globalStyles.detalheCartao}>
+          Reprodução: {item.reproduction}
+        </Text>
+      </View>
+    </View>
+  );
+
   return (
     <SafeAreaView style={globalStyles.container}>
-      <Text>Tela do reino plantas</Text>
+      <FlatList
+        data={plantas}
+        renderItem={plantasItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </SafeAreaView>
   );
 };
+
 
 export default Plantas;
